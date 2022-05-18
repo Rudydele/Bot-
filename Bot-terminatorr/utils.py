e188e6c91ce7af6b101e6d5b3880a35cb277bd6c
Rudy <rudydleon@ufm.edu>
2022-05-17 20:21:36 -0600

    Mi trabajo porque el otro me dio error

---
+++ Bot-terminatorr/utils.py
@@ -0,0 +1,257 @@
+"""
+[Module] Tic-tac-toe bot utilities.
+"""
+from random import randint
+import requests
+from urllib.parse import unquote
+
+
+API_URL = "http://127.0.0.1:8000"
+
+
+def is_registry_open() -> bool:
+    """
+    Checks if registry is available via API.
+    """
+    try:
+        url = "{}/registry".format(API_URL)
+        res = requests.get(url)
+
+        if res.text == "true":
+            return True
+        elif res.text == "false":
+            return False
+
+    except:
+        return False
+
+
+def register_user(name: str) -> str:
+    """
+    Registers user in API game.
+    """
+    url = "{}/register_player/{}".format(API_URL, name)
+    res = requests.post(url)
+    player_id = res.text[1]
+    return player_id
+
+
+def is_my_turn(player_id: str) -> bool: 
+    """
+    Checks if it is our turn via API.
+    """
+    url = "{}/turn/{}".format(API_URL, player_id)
+    res = requests.get(url)
+    
+    if res.text == "true":
+        return True
+    elif res.text == "false":
+        return False
+
+
+def read_board() -> list:
+    """
+    Gets game board via API.
+    """
+    url = "{}/board".format(API_URL)
+    res = requests.get(url)
+    board_str = res.text
+    board = [
+        [board_str[1], board_str[2], board_str[3]], 
+        [board_str[4], board_str[5], board_str[6]], 
+        [board_str[7], board_str[8], board_str[9]]
+    ]
+
+    return board
+
+
+def decide_move(board: list, player_id: str) -> list:
+    """
+    Decides next move to make.
+    """
+    malo = "0"
+    if player_id is "0":
+        malo = "X"
+
+    #Centro es clave, en caso de perder el centro busco la esquina
+    if board [1][1] == "-":
+        return [1][1]
+    if board [1][1] == malo and board [2][2] == "-":
+        return [2][2]
+
+    #Asegurar victoria
+    if board [0][0] == player_id and board[1][0] == player_id and [2][0] == "-":
+        return [2][0] 
+    if board [0][1] == player_id and board[1][1] == player_id and [2][1] == "-":
+        return [2][1] 
+    if board [0][2] == player_id and board[1][2] == player_id and [2][2] == "-":
+        return [2][2] 
+    #Asegurar victoria vertical abajo para arriba
+    if board [2][0] == player_id and board[1][0] == player_id and [0][0] == "-":
+        return [0][0] 
+    if board [2][1] == player_id and board[1][1] == player_id and [0][1] == "-":
+        return [0][1] 
+    if board [2][2] == player_id and board[1][2] == player_id and [0][2] == "-":
+        return [0][2] 
+    #Asegurar victoria vertical cubriendo centro
+    if board [2][0] == player_id and board[0][0] == player_id and [1][0] == "-":
+        return [1][0] 
+    if board [2][1] == player_id and board[0][1] == player_id and [1][1] == "-":
+        return [1][1] 
+    if board [2][2] == player_id and board[0][2] == player_id and [1][2] == "-":
+        return [1][2] 
+    #Asegurar victoria horizontal izquierda a derecha
+    if board [0][0] == player_id and board[0][1] == player_id and [0][2] == "-":
+        return [0][2] 
+    if board [1][0] == player_id and board[1][1] == player_id and [1][2] == "-":
+        return [1][2] 
+    if board [2][0] == player_id and board[2][1] == player_id and [2][2] == "-":
+        return [2][2] 
+    #Asegurar victoria horizontal derecha a izquierda
+    if board [0][2] == player_id and board[0][1] == player_id and [0][0] == "-":
+        return [0][0] 
+    if board [1][2] == player_id and board[1][1] == player_id and [1][0] == "-":
+        return [1][0] 
+    if board [2][2] == player_id and board[2][1] == player_id and [2][0] == "-":
+        return [2][0] 
+    #Asegurar victoria horizontal cubriendo centros
+    if board [0][0] == player_id and board[0][2] == player_id and [0][1] == "-":
+        return [0][1] 
+    if board [1][0] == player_id and board[1][2] == player_id and [1][1] == "-":
+        return [1][1] 
+    if board [2][0] == player_id and board[2][2] == player_id and [2][1] == "-":
+        return [2][1] 
+    #Asegurar victoria diagonal 1 de abajo para arriba
+    if board [2][0] == player_id and board[1][1] == player_id and [0][2] == "-":
+        return [0][2] 
+    #Asegurar victoria diagonal 1 de arriba para abajo
+    if board [0][2] == player_id and board[1][1] == player_id and [2][0] == "-":
+        return [2][0] 
+    #Asegurar victoria diagonal 1 cubriendo centro
+    if board [2][0] == player_id and board[0][2] == player_id and [1][1] == "-":
+        return [1][1] 
+    #Asegurar victoria diagonal 2 de abajo para arriba
+    if board [2][2] == player_id and board[1][1] == player_id and [0][0] == "-":
+        return [0][0] 
+    #Asegurar victoria diagonal 2 arriba para abajo
+    if board [0][0] == player_id and board[1][1] == player_id and [2][2] == "-":
+        return [2][2] 
+    #Asegurar victoria diagonal 2 cubriendo centros
+    if board [2][2] == player_id and board[0][0] == player_id and [1][1] == "-":
+        return [1][1] 
+
+    #Bloqueo bloqueo
+    #Bloqueo vertical arriba para abajo
+    if board [0][0] == malo and board[1][0] == malo and [2][0] == "-":
+        return [2][0] 
+    if board [0][1] == malo and board[1][1] == malo and [2][1] == "-":
+        return [2][1] 
+    if board [0][2] == malo and board[1][2] == malo and [2][2] == "-":
+        return [2][2] 
+    #Bloqueo vertical abajo para arriba
+    if board [2][0] == malo and board[1][0] == malo and [0][0] == "-":
+        return [0][0] 
+    if board [2][1] == malo and board[1][1] == malo and [0][1] == "-":
+        return [0][1] 
+    if board [2][2] == malo and board[1][2] == malo and [0][2] == "-":
+        return [0][2] 
+    #Bloqueo vertical cubriendo centro
+    if board [2][0] == malo and board[0][0] == malo and [1][0] == "-":
+        return [1][0] 
+    if board [2][1] == malo and board[0][1] == malo and [1][1] == "-":
+        return [1][1] 
+    if board [2][2] == malo and board[0][2] == malo and [1][2] == "-":
+        return [1][2] 
+    #Bloqueo horizontal izquierda a derecha
+    if board [0][0] == malo and board[0][1] == malo and [0][2] == "-":
+        return [0][2] 
+    if board [1][0] == malo and board[1][1] == malo and [1][2] == "-":
+        return [1][2] 
+    if board [2][0] == malo and board[2][1] == malo and [2][2] == "-":
+        return [2][2] 
+    #Bloqueo horizontal derecha a izquierda
+    if board [0][2] == malo and board[0][1] == malo and [0][0] == "-":
+        return [0][0] 
+    if board [1][2] == malo and board[1][1] == malo and [1][0] == "-":
+        return [1][0] 
+    if board [2][2] == malo and board[2][1] == malo and [2][0] == "-":
+        return [2][0] 
+    #Bloqueo horizontal cubriendo centros
+    if board [0][0] == malo and board[0][2] == malo and [0][1] == "-":
+        return [0][1] 
+    if board [1][0] == malo and board[1][2] == malo and [1][1] == "-":
+        return [1][1] 
+    if board [2][0] == malo and board[2][2] == malo and [2][1] == "-":
+        return [2][1] 
+    #Bloqueo diagonal 1 de abajo para arriba
+    if board [2][0] == malo and board[1][1] == malo and [0][2] == "-":
+        return [0][2] 
+    #Bloqueo diagonal 1 de arriba para abajo
+    if board [0][2] == malo and board[1][1] == malo and [2][0] == "-":
+        return [2][0] 
+    #Bloqueo diagonal 1 cubriendo centro
+    if board [2][0] == malo and board[0][2] == malo and [1][1] == "-":
+        return [1][1] 
+    #Bloqueo diagonal 2 de abajo para arriba
+    if board [2][2] == malo and board[1][1] == malo and [0][0] == "-":
+        return [0][0] 
+    #Bloqueo diagonal 2 arriba para abajo
+    if board [0][0] == malo and board[1][1] == malo and [2][2] == "-":
+        return [2][2] 
+    #Bloqueo diagonal 2 cubriendo centros
+    if board [2][2] == malo and board[0][0] == malo and [1][1] == "-":
+        return [1][1] 
+
+
+    #Mis movimientos (fuera de asegurar victoria, bloquear, buscar centro y buscar una esquina) no son importantes
+    x = randint(0,2)
+    y = randint(0, 2)
+    return [x, y]
+
+
+
+def validate_move(board: list, move: list) -> bool:
+    """
+    Checks if the desired next move hits an empty position.
+    """
+    row, col = move[0], move[1]
+
+    if board[row][col] == "-":
+        return True
+
+    return False
+
+
+def send_move(player_id: str, move: list) -> None:
+    """
+    Sends move to API.
+    """
+    row, col = move[0], move[1]
+    url = "{}/move/{}/{}/{}".format(API_URL, player_id, row, col)
+    res = requests.post(url)
+    return None
+
+
+def does_game_continue() -> bool:
+    """
+    Checks if the current match continues via API.
+    """
+    url = "{}/continue".format(API_URL)
+    res = requests.get(url)
+
+    if res.text == "true":
+        return True
+    elif res.text == "false":
+        return False
+
+
+def print_board(board: list) -> None:
+    '''
+    Prints the baord in console to watch the game.
+    '''
+    print("\nCurrent board: \n")
+    print(board[0][0], "|", board[0][1], "|", board[0][2])
+    print("----------")
+    print(board[1][0], "|", board[1][1], "|", board[1][2])
+    print("----------")
+    print(board[2][0], "|", board[2][1], "|", board[2][2], "\n")
